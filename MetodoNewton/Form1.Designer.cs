1660321815303b5a1e4e86627f4cf6f96636828f
Yael Matamoros <yaelguzman750@gmail.com>
2024-10-27 16:43:40 -0600

    Agregar archivos de proyecto.

---
+++ MetodoNewton/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace MetodoNewton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnComenzar = new System.Windows.Forms.Button();
+            this.inputNumPuntos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textPolinomio = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(278, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Metodo de Newton";
+            // 
+            // btnComenzar
+            // 
+            this.btnComenzar.Location = new System.Drawing.Point(75, 176);
+            this.btnComenzar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnComenzar.Name = "btnComenzar";
+            this.btnComenzar.Size = new System.Drawing.Size(72, 19);
+            this.btnComenzar.TabIndex = 1;
+            this.btnComenzar.Text = "Comenzar";
+            this.btnComenzar.UseVisualStyleBackColor = true;
+            this.btnComenzar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputNumPuntos
+            // 
+            this.inputNumPuntos.Location = new System.Drawing.Point(73, 145);
+            this.inputNumPuntos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inputNumPuntos.Name = "inputNumPuntos";
+            this.inputNumPuntos.Size = new System.Drawing.Size(75, 20);
+            this.inputNumPuntos.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 115);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numero de Puntos";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(238, 93);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(546, 292);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 227);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "x";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(134, 227);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "f(x)";
+            // 
+            // textPolinomio
+            // 
+            this.textPolinomio.AutoSize = true;
+            this.textPolinomio.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPolinomio.Location = new System.Drawing.Point(235, 419);
+            this.textPolinomio.Name = "textPolinomio";
+            this.textPolinomio.Size = new System.Drawing.Size(69, 13);
+            this.textPolinomio.TabIndex = 11;
+            this.textPolinomio.Text = "Polinomio: ";
+            this.textPolinomio.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 526);
+            this.Controls.Add(this.textPolinomio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputNumPuntos);
+            this.Controls.Add(this.btnComenzar);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnComenzar;
+        private System.Windows.Forms.TextBox inputNumPuntos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label textPolinomio;
+    }
+}
+
---
+++ MetodoNewton/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetodoNewton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int numeroPuntos = int.Parse(inputNumPuntos.Text);
+            int puntoMover = 262;
+            
+            for (int i = 0; i < numeroPuntos; i++)
+            {
+                TextBox puntoX = new TextBox();
+                puntoX.Name = "puntoX" + i.ToString();
+                puntoX.Size = new Size(66, 22);
+                puntoX.Location = new Point(48,puntoMover);
+
+                TextBox puntoFx = new TextBox();
+                puntoFx.Name = "puntoFx" + i.ToString();
+                puntoFx.Size = new Size(66, 22);
+                puntoFx.Location = new Point(125, puntoMover);
+                puntoFx.Enabled = true;
+
+                puntoMover += 42;
+
+                this.Controls.Add(puntoX);
+                this.Controls.Add(puntoFx);
+            }
+
+            /*Creacion del boton para resolver*/
+            Button botonResolver = new Button();
+            botonResolver.Text = "Resolver";
+            botonResolver.Name = "btnResolver";
+            botonResolver.Location = new Point(76,puntoMover);
+            botonResolver.Size = new Size(72, 19);
+            this.Controls.Add(botonResolver);
+
+            // Asociar el evento Click al método manejador
+            botonResolver.Click += new EventHandler(botonResolver_Click);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contenedorPuntos_Enter(object sender, EventArgs e)
+        {
+
+        }
+        // Método manejador del evento Click para el botón "Resolver"
+        private void botonResolver_Click(object sender, EventArgs e)
+        {
+            // Limpiar el DataGridView antes de configurarlo
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+
+            /*INICIO DE LLENADO DE LAS MATRIZES*/
+            double[] matrizPuntosX = new double[int.Parse(inputNumPuntos.Text)];
+            double[] matrizPuntosY = new double[int.Parse(inputNumPuntos.Text)];
+
+
+            // Agregar columnas
+            dataGridView1.Columns.Add("x", "x");
+            dataGridView1.Columns.Add("f(x)", "f(x)");
+            for (int i = 0; i < int.Parse(inputNumPuntos.Text); i++)
+            {
+                // Buscar el TextBox por su nombre
+                TextBox txtPuntos = this.Controls.Find("puntoX" + i.ToString(), true).FirstOrDefault() as TextBox;
+                matrizPuntosX[i] = int.Parse(txtPuntos.Text);
+                // Buscar el TextBox por su nombre
+                TextBox txtPuntos2 = this.Controls.Find("puntoFx" + i.ToString(), true).FirstOrDefault() as TextBox;
+                matrizPuntosY[i] = int.Parse(txtPuntos2.Text);
+
+                dataGridView1.Rows.Add(matrizPuntosX[i], matrizPuntosY[i]);
+            }
+            /*FIN DE LLENADO DE LAS MATRIZES*/
+
+            /*RESOLUCION DE TODAS LAS Y1*/
+            int aumento = 0;
+            int reducirYveces = 1;
+            for (int i = 0; i < int.Parse(inputNumPuntos.Text) - 1; i++) {
+                
+                dataGridView1.Columns.Add("y"+(i+1), "y" + (i + 1));
+                for (int j = 0; j < int.Parse(inputNumPuntos.Text) - reducirYveces; j++)
+                {
+                    if (i == 0)
+                    {
+                        if (j == 0) /*con esto vamos agregando el polinomio*/
+                        {
+                            textPolinomio.Text += matrizPuntosY[0].ToString();
+                        }
+                        
+                        /*Formula para calcular cada Y*/
+                        matrizPuntosY[j] = (matrizPuntosY[j + 1] - matrizPuntosY[j]) / (matrizPuntosX[j + 1] - matrizPuntosX[j]);
+
+                        /*ahora llenaremos el datagridview*/
+                        foreach (DataGridViewRow fila in dataGridView1.Rows)
+                        {
+                                // Verificar si el valor de la fila actual es el que buscamos
+                            if (fila.Cells["x"].Value != null && fila.Cells["x"].Value.ToString() == matrizPuntosX[j].ToString())
+                            {
+                               // agregar la y1
+                                fila.Cells["y"+(i+1)].Value = matrizPuntosY[j].ToString();
+                                break; // Salir del bucle después de encontrar y actualizar
+                            }
+                        }
+                        if (j == 0) /*con esto vamos agregando el polinomio*/
+                        {
+                            textPolinomio.Text += " + ("+ matrizPuntosY[0].ToString() + ")*(x - (" + matrizPuntosX[0] +"))";
+                        }
+
+                    }
+                    else
+                    {
+                        if(i == 1) /*esto se usa para la formula la cual aqui el objetivo es lograr llegar a la ultima posicion de las x q se usan en la formula*/
+                        {          /*por ej si se tiene x0 con este 2 logramos llegar a la x2 para agarrar su dato ahora bien*/
+                                   /*si ahora se necesita calcular el otro punto q es x1 para la y2 ahora necesitamos la x3 en la formula con lo cual necesito aumentar 2 posiciones para llegar a la x3 las y*/
+                            aumento = 2;
+                        }
+
+                        matrizPuntosY[j] = (matrizPuntosY[j + 1] - matrizPuntosY[j])/ (matrizPuntosX[j + aumento] - matrizPuntosX[j]);
+                        
+                        /*ahora llenaremos el datagridview*/
+                        foreach (DataGridViewRow fila in dataGridView1.Rows)
+                        {
+                            // Verificar si el valor de la fila actual es el que buscamos
+                            if (fila.Cells["x"].Value != null && fila.Cells["x"].Value.ToString() == matrizPuntosX[j].ToString())
+                            {
+                                // agregar la y1
+                                fila.Cells["y" + (i + 1)].Value = matrizPuntosY[j].ToString();
+                                break; // Salir del bucle después de encontrar y actualizar
+                            }
+                        }
+
+                        textPolinomio.Text += " + (" + matrizPuntosY[0].ToString() + ")*(x - (" + matrizPuntosX[0] + "))(x -("+ matrizPuntosX[1] + "))";
+                    }
+                }
+                aumento += 1; /*aqui ya se hace un aumento de a 1 dad que despues solo va aumentando de 1 en 1 para llegar por ej si estamos en y3*/
+                reducirYveces += 1; /*y estamos en x0 ahora la primera vez queremos llegar al valor de x3 entonces cuando le aumentamo 1 q anteriormente tenia 2 pues sera capaz de llegar a ese valor*/
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
